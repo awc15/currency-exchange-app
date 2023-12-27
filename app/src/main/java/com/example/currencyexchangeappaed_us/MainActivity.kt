4f98b71204514d972538bbf4e5896a24cb1d48dd
AWC <abdulwahabch30@gmail.com>
2023-12-27 13:23:34 +0500

    Enabled ViewBinding and Remove action bar

--- app/src/main/java/com/example/currencyexchangeappaed_us/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchangeappaed_us/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.currencyexchangeappaed_us
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.currencyexchangeappaed_us.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.let { bind ->
+            setContentView(bind.root)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
